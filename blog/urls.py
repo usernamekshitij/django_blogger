73d90b48961a6761eb6edc06973af5bb7ea8b08a
usernamekshitij <usernamekshitij@gmail.com>
2019-05-25 13:22:19 +0530

    This is update version of blogging app

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,15 @@ from .views import (PostListView,
 	PostDetailView,
 	PostCreateView,
 	PostUpdateView,
-	PostDeleteView
-	)
+	PostDeleteView,
+	UserPostListView
+    )
 
 from . import  views
 
 urlpatterns = [
     path('', PostListView.as_view(),name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(),name='user-post'),
     path('about/', views.about,name='blog-about'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic import (DeleteView,
  ListView, DetailView,CreateView,UpdateView)
+
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,UserPassesTestMixin)
 
@@ -21,8 +23,20 @@ class PostListView(ListView):
     model=Post
     template_name='blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name='posts'
-    ordering=['-date_posted']
     paginate_by= 2
+    ordering=['-date_posted']
+    
+class UserPostListView(ListView):
+    model=Post
+    template_name='blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name='posts'
+    #ordering=['-date_posted']
+    paginate_by= 2
+    
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 
 class PostDetailView(DetailView):
